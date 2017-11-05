62b0fb31eaaa0b7001dee49f04ae02246c8046e4
IordacheTiberiu <32988554+IordacheTiberiu@users.noreply.github.com>
2017-11-05 23:18:22 +0200

    T1_ex1_Iordache_Tiberiu

---
+++ T1_ex1_Iordache_Tiberiu.m
@@ -0,0 +1,46 @@
+t=0:0.002:4; % Semnalul exista doar pentru perioada de timp [0,4]s, 
+             % iar rezolutia temporala este de 2ms.
+T=2;   % Perioada semnalului este de 2 secunde
+f=1/T; % Frecventa este 1/Perioada Hz
+
+x=0.75*square(2*pi*f*t,25)-0.25; 
+% x este variabila ce reprezinta semnalul.
+% Deoarece functia square genereaza un semnal dreptunghiular de amplitudine 1 si
+%  simetric fata de origine, am ales sa il inmultesc cu 0.75 pentru a obtine
+%  amplitudinea varf la varf de 1.5 de care am nevoie, si sa scad 0.25 pentru
+%  a seta intervalul amplitudinii de la [0.75,-0.75]V la [0.5,-1]V.
+% Primul argument al functiei este omega*t, omega fiind egal cu 2*pi*f.
+% Al doilea argument primit de funtia square(25) reprezinta fatorul de
+%  umplere.
+subplot(3,1,1);
+% Functia subplot imi permite sa reprezint intr o singura fereastra toate
+%  cele 3 semnale care difera in functie de rezolutia temporala.
+plot(t,x),grid,xlabel('Timp(s)'),ylabel('Amplitudine(V)'),axis([0 4 -1.5 1]),title('Semnal Dreptunghiular 2ms'); 
+% Axa ox reprezinta timpul in secunde, axa oy reprezinta amplitudinea in
+% volti.
+
+% Similar pentru celelalte doua cazuri, rezolutia temporala fiind singul
+%  lucru care se schimba
+
+t=0:0.02:7;
+
+T=2;
+f=1/T;  
+
+x=0.75*square(2*pi*f*t,25)-0.25; 
+subplot(3,1,2);
+plot(t,x),grid,xlabel('Timp(s)'),ylabel('Amplitudine(V)'),axis([0 4 -1.5 1]),title('Semnal Dreptunghiular 20ms');
+
+t=0:0.2:7;
+
+T=2;
+f=1/T; 
+
+x=0.75*square(2*pi*f*t,25)-0.25; 
+subplot(3,1,3);
+plot(t,x),grid,xlabel('Timp(s)'),ylabel('Amplitudine(V)'),axis([0 4 -1.5 1]),title('Semnal Dreptunghiular 200ms');
+
+
+
+
+
